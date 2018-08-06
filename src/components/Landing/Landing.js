3cae006a4d91327f4fb43ea5c50abe2137dfd6ba
user1 <user1@user1s-MacBook-Pro.local>
2018-08-06 14:42:31 -0500

    added landing page

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <button>Login</button>
+        <div>
+          Find a Room
+          <button>Register</button>
+        </div>
+        <div>
+          Rent a Room
+          <button>Register</button>
+        </div>
+      </div>
+    );
+  }
+}
