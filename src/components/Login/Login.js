d183fc22e8dec1e1506ba5e16cef85bc82ffafbb
Blake Engquist <engquist_blake@yahoo.com>
2018-08-06 15:13:27 -0500

    fixed errors

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
-import React from "../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react";
-import { Link } from "../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-router-dom";
-import { connect } from "../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-redux";
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { getItems } from "../../redux/ducks/userReducer";
 
 const Login = props => {
