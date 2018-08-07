1f37ede960dff526714caa41f7f3011c1c87c232
Rana Mohammad <ranamohammad@Ranas-MacBook-Pro.local>
2018-08-07 10:47:42 -0500

    initial

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../../App";
+import { Link } from "react-router-dom";
+
+
+class Nav extends Component {
+
+
+
+
+  render(){
+
+    return (
+
+      <div>nav</div>
+    )
+  }
+}
+export default Nav;
