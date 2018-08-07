7ae141f785dc8f3cdb7d84936bb2e79cdd5f2505
user1 <user1@user1s-MacBook-Pro.local>
2018-08-07 10:32:04 -0500

    added mongoose

--- server/index.js
+++ server/index.js
@@ -70,11 +70,6 @@ passport.serializeUser((user, done) => {
     .catch(console.log);
 });
 
-//Logs user out of session
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
