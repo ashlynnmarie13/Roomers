d9afd0b705f9c1841bf22373eb8e4ac07d0c3f66
Rana Mohammad <ranamohammad@Ranas-MacBook-Pro.local>
2018-08-08 09:15:26 -0500

    working

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Nav.css";
 import "../../App";
+import logo from "./logo_transparent.png";
 import { Link } from "react-router-dom";
 
 class Nav extends Component {
@@ -8,24 +9,24 @@ class Nav extends Component {
     return (
       <div className="Nav">
         <div>
-          <Link to="/profile">
+          <Link to="/profile" className='logo'>
             <img src="" alt="logo" />
           </Link>
 
-          <Link to="/wishlist">
+          <Link to="/wishlist" className="wishlist">
             <i className="WishList" /> Wish List
           </Link>
         </div>
         {/* <p>ROOMERS</p> */}
-        <Link to="/home">ROOMERS</Link>
+        <Link to="/home" className='home'><img src={logo} alt="logo" /></Link>
         <div className='listings'>
-          <Link to="/addlisting">
+          <Link to="/addlisting" className="addlistings">
             <i className="Addlistings" /> Add New Listings
           </Link>
-          <Link to="/searchrooms">
+          <Link to="/searchrooms" className="searchlistings">
             <i className="Searchlistings" /> Serch Listings
           </Link>
-          <Link to="/mylistings">
+          <Link to="/mylistings" className="mylistings">
             <i className="Mylistings" /> My Listings
           </Link>
         </div>
@@ -34,3 +35,4 @@ class Nav extends Component {
   }
 }
 export default Nav;
+
