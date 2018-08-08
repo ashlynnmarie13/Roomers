27e73570f26574d7d8e5caf7a57ac9e927a453c2
Rana Mohammad <ranamohammad@Ranas-MacBook-Pro.local>
2018-08-08 09:41:45 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
-import Login from "./components/Login/Login";
 import routes from "./routes";
 import "./reset.css";
 import "./App.css";
-<<<<<<< HEAD
-import Nav from "./components/Nav/Nav"
-import {withRouter} from 'react-router-dom'
-import Hom from './components/Hom/Hom'
-=======
+import { withRouter } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
-import Hom from "./components/Hom/Hom";
 import { connect } from "react-redux";
 import { getUser } from "./redux/ducks/userReducer";
->>>>>>> master
-import Landing from "./components/Landing/Landing";
 
 class App extends Component {
   componentDidMount() {
@@ -23,20 +15,16 @@ class App extends Component {
     return (
       <div className="App">
         <p>{JSON.stringify(this.props.user)}</p>
-
+        <Nav />
         {routes}
       </div>
     );
   }
 }
 
-<<<<<<< HEAD
-export default withRouter(App);
-=======
-const mapStateToProps = ({ user, state }) => ({ ...user, state });
-
-export default connect(
-  mapStateToProps,
-  { getUser }
-)(App);
->>>>>>> master
+export default withRouter(
+  connect(
+    state => state,
+    { getUser }
+  )(App)
+);
