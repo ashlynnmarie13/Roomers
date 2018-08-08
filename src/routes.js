761daffd5df0328a7ca20bf9e8a54250c4ca8d95
cris93g <37388115+cris93g@users.noreply.github.com>
2018-08-08 09:45:10 -0500

    oops

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import Landing from "./components/Landing/Landing";
-<<<<<<< HEAD
-import Location from "./components/Location/Location";
-=======
 import Chat from "./components/Chat/Chat";
 import Home from "./components/Home/Home";
->>>>>>> master
 
 export default (
   <Switch>
