721ddb8eeaa8f1551e643b8415fddb4ebac1c4bc
Blake Engquist <engquist_blake@yahoo.com>
2018-08-10 10:27:04 -0500

    idk

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,34 +9,46 @@ class Nav extends Component {
     return (
       <div className="Nav">
         <div>
-          
-          < NavLink to="/profile" className='logo'>
+          <NavLink to="/profile" className="logo">
             <img src="" alt="logo" />
           </NavLink>
-          <NavLink to="/mylistings" className="mylistings" activeStyle={{color: '#CF6766'}}>
+          <NavLink
+            to="/mylistings"
+            className="mylistings"
+            activeStyle={{ color: "#CF6766" }}
+          >
             <i className="Mylistings" /> My Listings
           </NavLink>
 
-          <NavLink to="/wishlist" className="wishlist" activeStyle={{color: '#CF6766'}}>
+          <NavLink
+            to="/wishlist"
+            className="wishlist"
+            activeStyle={{ color: "#CF6766" }}
+          >
             <i activeClassName="WishList" /> Wish List
           </NavLink>
-          
         </div>
         {/* <p>ROOMERS</p> */}
-        <Link to="/home" className='home'><img src={logo} alt="logo" /></Link>
-        <div className='listings'>
-          <NavLink to="/searchrooms" className="searchlistings" activeStyle={{color: '#CF6766'}}>
+        <Link to="/home" className="home">
+          <img src={logo} alt="logo" />
+        </Link>
+        <div className="listings">
+          <NavLink
+            to="/searchrooms"
+            className="searchlistings"
+            activeStyle={{ color: "#CF6766" }}
+          >
             <i activeClassName="Searchlistings" /> Search Listings
           </NavLink>
           <Link to="/addlisting" className="addlistings">
             <i className="Addlistings" /> Add New Listing
           </Link>
-          <Link to="/chat" ><i class="fa fa-comment" aria-hidden="true"></i></Link>
-         
+          <Link to="/chat">
+            <i class="fa fa-comment" aria-hidden="true" />
+          </Link>
         </div>
       </div>
     );
   }
 }
 export default Nav;
-
